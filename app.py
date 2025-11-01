2b97d4b42b165fe2b87e63ffb35664bb625cf136
yeschirag <chiragsharma.ijs@gmail.com>
2025-11-02 03:02:17 +0530

    chal jaa bhai

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -70,4 +71,5 @@ def check_answer():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)  
